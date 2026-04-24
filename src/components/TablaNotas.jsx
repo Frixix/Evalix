c669e0bfc32a3513f08de4660f6149c5ec8b89aa
frix <robinsonestibenrodriguez1234@gmail.com>
2026-04-24 09:00:25 -0500

    Feat : ahora la tabla guarda los cambios

--- src/components/TablaNotas.jsx
+++ src/components/TablaNotas.jsx
@@ -1,6 +1,26 @@
 import { estudiantesMock } from "../data/estudiantes";
+import { useState } from "react";
 
 function TablaNotas() {
+  const [estudiantes, setEstudiantes] = useState(estudiantesMock);
+
+    const handleNotaChange = (id, actividad, valor) => {
+    const nuevosEstudiantes = estudiantes.map((est) => {
+      if (est.id === id) {
+        return {
+          ...est,
+          notas: {
+            ...est.notas,
+            [actividad]: valor
+          }
+        };
+      }
+      return est;
+    });
+
+    setEstudiantes(nuevosEstudiantes);
+  };
+
   return (
     <div>
       <h2>Tabla de Notas</h2>
@@ -15,21 +35,34 @@ function TablaNotas() {
         </thead>
 
         <tbody>
-          {estudiantesMock.map((est) => (
+          {estudiantes.map((est) => (
             <tr key={est.id}>
               <td>{est.nombre}</td>
 
               <td>
-                <input type="number" />
+                <input
+                  type="number"
+                  value={est.notas.actividad1}
+                  onChange={(e) =>
+                    handleNotaChange(est.id, "actividad1", e.target.value)
+                  }
+                />
               </td>
 
               <td>
-                <input type="number" />
+                <input
+                  type="number"
+                  value={est.notas.actividad2}
+                  onChange={(e) =>
+                    handleNotaChange(est.id, "actividad2", e.target.value)
+                  }
+                />
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <pre>{JSON.stringify(estudiantes, null, 2)}</pre>
     </div>
   );
 }
--- src/data/estudiantes.js
+++ src/data/estudiantes.js
@@ -1,4 +1,18 @@
 export const estudiantesMock = [
-  { id: 1, nombre: "Juan Rodríguez" },
-  { id: 2, nombre: "María Casallas" }
+  {
+    id: 1,
+    nombre: "Juan Rodríguez",
+    notas: {
+      actividad1: "",
+      actividad2: ""
+    }
+  },
+  {
+    id: 2,
+    nombre: "María Casallas",
+    notas: {
+      actividad1: "",
+      actividad2: ""
+    }
+  }
 ];
